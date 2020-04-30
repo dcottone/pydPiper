d36b58575b4f23625671799db1d2f585616cba22
dcottone <53377005+dcottone@users.noreply.github.com>
2020-05-01 00:09:12 +0200

    Update DCAudio_NS1_left.py

--- DCAudio_NS1_left.py
+++ DCAudio_NS1_left.py
@@ -20,6 +20,7 @@ TRUETYPE_FONTS = {
 	'GraphikThin28': { 'file':'/app/fonts/GraphikThin.otf', 'size':28 },
     'GraphikMedium18': { 'file':'/app/fonts/GraphikMedium.otf', 'size':18 },
 	'GraphikRegular12': { 'file':'/app/fonts/GraphikRegular.otf', 'size':12 },
+	'GraphikThin12': { 'file':'/app/fonts/GraphikThin.otf', 'size':12 },
 }
 
 IMAGES = {
@@ -33,13 +34,13 @@ WIDGETS = {
 	'splashStreamer': { 'type':'ttext', 'format':'Streamer', 'font':'GraphikThin28' },
 	'artist': { 'type':'ttext', 'format':'{0}', 'variables':['artist'], 'font':'GraphikMedium18', 'just':'center', 'effect':('scroll','left',1,1,20,'onloop',3,128), 'size':(128,18)},
 	'bitdepth': { 'type':'ttext', 'format':'{0}', 'variables':['bitdepth'], 'font':'GraphikRegular12', 'just':'center','varwidth':True},
-	'samplerate': { 'type':'ttext', 'format':'{0}', 'variables':['samplerate'], 'font':'GraphikRegular12', 'just':'center','varwidth':True},
+	'samplerate': { 'type':'ttext', 'format':'{0}', 'variables':['samplerate'], 'font':'GraphikThin12', 'just':'center','varwidth':True},
 }
 
 # Assemble the widgets into canvases.  Only needed if you need to combine multiple widgets together so you can produce effects on them as a group.
 CANVASES = {
 	'splashLogo': { 'widgets': [ ('splashDCaudio',1,1), ('splashStreamer',3,35) ], 'size':(128,64) },
-	'playing': { 'widgets': [ ('artist',0,23) , ('bitdepth',0,52), ('samplerate',50,52)], 'size':(128,64) },
+	'playing': { 'widgets': [ ('artist',0,23) , ('bitdepth',0,52), ('samplerate',40,52)], 'size':(128,64) },
 	'blank': { 'widgets': [], 'size':(128,64) },
 }
 
