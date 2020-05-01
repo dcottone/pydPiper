4385422b4e240e843abcd8d51e65f74c8a4ddbc9
dcottone <53377005+dcottone@users.noreply.github.com>
2020-05-01 21:55:58 +0200

    Update DCAudio_NS1_left.py

--- DCAudio_NS1_left.py
+++ DCAudio_NS1_left.py
@@ -33,14 +33,15 @@ WIDGETS = {
 	'splashDCaudio': { 'type':'ttext', 'format':'DCaudio', 'font':'GraphikBold28' },
 	'splashStreamer': { 'type':'ttext', 'format':'Streamer', 'font':'GraphikThin28' },
 	'artist': { 'type':'ttext', 'format':'{0}', 'variables':['artist'], 'font':'GraphikMedium18', 'just':'center', 'effect':('scroll','left',1,1,20,'onloop',3,128), 'size':(128,18)},
-	'bitdepth': { 'type':'text', 'format':'{0}', 'variables':['bitdepth'], 'font':'small', 'just':'center','varwidth':True},
+	'bitdepth': { 'type':'text', 'format':'{0}', 'variables':['bitdepth'], 'font':'small', 'just':'left','varwidth':True},
 	'samplerate': { 'type':'text', 'format':'{0}', 'variables':['samplerate'], 'font':'small', 'just':'center','varwidth':True,'size':(40,8)},
+	'encoding': { 'type':'text', 'format':'{0}', 'variables':['encoding'], 'font':'small', 'just':'right','varwidth':True,'size':(40,8)},
 }
 
 # Assemble the widgets into canvases.  Only needed if you need to combine multiple widgets together so you can produce effects on them as a group.
 CANVASES = {
 	'splashLogo': { 'widgets': [ ('splashDCaudio',1,1), ('splashStreamer',3,35) ], 'size':(128,64) },
-	'playing': { 'widgets': [ ('artist',0,23) , ('bitdepth',0,56), ('samplerate',44,56)], 'size':(128,64) },
+	'playing': { 'widgets': [ ('artist',0,23) , ('bitdepth',0,56), ('samplerate',44,56) , ('encoding',88,56)], 'size':(128,64) },
 	'blank': { 'widgets': [], 'size':(128,64) },
 }
 
@@ -53,9 +54,9 @@ CANVASES = {
 # To access system variables, refer to them within the db dictionary (e.g. db['title'])
 # To access the most recent previous state of a variable, refer to them within the dbp dictionary (e.g. dbp['title'])
 SEQUENCES = [
-	{       
-		'name': 'seqSplash', 
-		'canvases': [ { 'name':'splashLogo', 'duration':30 } ], 
+	{
+		'name': 'seqSplash',
+		'canvases': [ { 'name':'splashLogo', 'duration':30 } ],
 		'conditional':"db['state']=='starting'"
 	},
 	{
